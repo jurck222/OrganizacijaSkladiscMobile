45f2210abb068a373bba571afd53ef3147161cbe
jurck222 <56190152+jurck222@users.noreply.github.com>
2023-01-11 18:06:29 +0100

    added post for orders

--- app/src/main/java/com/example/organizacijaskladi/MainActivity.java
+++ app/src/main/java/com/example/organizacijaskladi/MainActivity.java
@@ -6,95 +6,214 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private RequestQueue requestQueue;
-    private TextView articles;
+    private Button send;
+    private Button cancle;
+    private Spinner sklad;
+    private TextView orderCode;
+    private TextView codeA;
+    private TextView name;
+    private EditText num;
+    private EditText z;
+    private String zone;
     private String url="https://warehouse-organizer-is.azurewebsites.net/api/v1/Article";
     private String url1="https://warehouse-organizer-is.azurewebsites.net/api/v1/Order";
     private String url2="https://warehouse-organizer-is.azurewebsites.net/api/v1/Warehouse";
+    public ArrayList<String> filelist = new ArrayList<>();
+    public ArrayList<String> whouses = new ArrayList<>() ;
+    public String orderCodes="";
+    public String desc="";
+    public String skladisce="";
+    public int quantity=0;
+    public int code=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestQueue=Volley.newRequestQueue(getApplicationContext());
-        articles = findViewById(R.id.Articles);
+        orderCode = findViewById(R.id.orderCode);
+        codeA = findViewById(R.id.code);
+        z=findViewById(R.id.zone);
+        name = findViewById(R.id.name);
+        num = findViewById(R.id.num);
+        filelist =  (ArrayList<String>)getIntent().getSerializableExtra("FILES_TO_SEND");
+        whouses =  (ArrayList<String>)getIntent().getSerializableExtra("FILES_TO_SEND2");
+        showArticles();
+        System.out.println(whouses.size());
+        sklad = (Spinner) findViewById(R.id.skladisca);
+        sklad.setOnItemSelectedListener(this);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, whouses);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sklad.setAdapter(dataAdapter);
+        cancle = findViewById(R.id.cancle);
+        cancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+        send = findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getStuff();
+
+            }
+        });
+
     }
-    public void showArticles(View view){
-        if(view != null){
+    public void showArticles(){
+
             JsonArrayRequest request = new JsonArrayRequest(url,jsonArrayListenerArticles,errorListener);
             requestQueue.add(request);
-        }
+
     }
 
-    public void showWhouses(View view){
-        if(view != null){
-            //articles.setText("Warhouses");
-            JsonArrayRequest request = new JsonArrayRequest(url2,jsonArrayListenerWarehouses,errorListener);
-            requestQueue.add(request);
-        }
-    }
+
     private Response.Listener<JSONArray>jsonArrayListenerArticles = new Response.Listener<JSONArray>() {
         @Override
         public void onResponse(JSONArray response) {
-            ArrayList<String> data = new ArrayList<>();
-            for(int i = 0; i<response.length(); i++){
-                try {
-                    JSONObject object = response.getJSONObject(i);
-                    int code = object.getInt("code");
-                    String desc = object.getString("description");
-                    int quantity = object.getInt("quantity");
-                    data.add(code+" "+desc+" "+quantity);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    return;
-                }
-            }
-            articles.setText("Articles");
-            for(String row:data){
-                String currTxt = articles.getText().toString();
-                articles.setText(currTxt+"\n\n"+row);
-            }
-        }
-    };
-    private Response.Listener<JSONArray>jsonArrayListenerWarehouses = new Response.Listener<JSONArray>() {
-        @Override
-        public void onResponse(JSONArray response) {
-            ArrayList<String> data = new ArrayList<>();
-            for(int i = 0; i<response.length(); i++){
-                try {
-                    JSONObject object = response.getJSONObject(i);
-                    String code = object.getString("warehouseCode");
-                    String desc = object.getString("zone");
 
-                    data.add(code+" "+desc);
+            orderCodes = filelist.get(0);
+            orderCode.setText(orderCodes);
+
+            for(int i = 0; i<response.length(); i++){
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    code = object.getInt("code");
+                    String text = filelist.get(1);
+                    text.replace("\n", "");
+                    if(code == Integer.parseInt(text)){
+                        desc = object.getString("description");
+                        quantity = object.getInt("quantity") * (filelist.size()-2);
+                        break;
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                     return;
                 }
-        }
-            articles.setText("Warhouses");
-            for(String row:data){
-                String currTxt = articles.getText().toString();
-                articles.setText(currTxt+"\n\n"+row);
             }
-    }
+            codeA.setText(String.valueOf(code));
+            name.setText(desc);
+
+            //articles.setText(orderCode+" "+code+" "+desc+" "+quantity);
+        }
     };
+
     private Response.ErrorListener errorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
             Log.d("REST error",error.getMessage());
         }
     };
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, ScannerActivity.class);
+
+        startActivity(switchActivityIntent);
+    }
+    private void getStuff(){
+        quantity+=Integer.parseInt(num.getText().toString());
+        zone=z.getText().toString();
+        addArticle();
+        switchActivities();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        String item = adapterView.getItemAtPosition(i).toString();
+        System.out.println(item);
+        skladisce=item;
+        // Showing selected spinner item
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    private void addArticle() {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = url1;
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("orderCode",orderCodes);
+            jsonBody.put("articleCode", code);
+            jsonBody.put("quantity", quantity);
+            jsonBody.put("warehouseName", skladisce);
+            jsonBody.put("whouseZone",zone);
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/organizacijaskladi/ScannerActivity.java
+++ app/src/main/java/com/example/organizacijaskladi/ScannerActivity.java
@@ -6,25 +6,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class ScannerActivity extends AppCompatActivity {
     private Button scan;
+    private Button finish;
     private TextView res;
+    private RequestQueue requestQueue;
+    private String url="https://warehouse-organizer-is.azurewebsites.net/api/v1/Article";
+    private String url1="https://warehouse-organizer-is.azurewebsites.net/api/v1/Order";
+    private String url2="https://warehouse-organizer-is.azurewebsites.net/api/v1/Warehouse";
+    private ArrayList<String> barcodes ;
+    private ArrayList<String>whouses;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        barcodes=new ArrayList<>();
+        whouses=new ArrayList<>();
+        requestQueue= Volley.newRequestQueue(getApplicationContext());
         setContentView(R.layout.activity_scanner);
         res = findViewById(R.id.scanRes);
         scan= findViewById(R.id.scaned);
+        finish = findViewById(R.id.finish);
+        showWhouses();
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,10 +70,31 @@ public class ScannerActivity extends AppCompatActivity {
         options.setCaptureActivity(CaptureAct.class);
         barlauncher.launch(options);
     }
+    public void showWhouses(){
+
+        JsonArrayRequest request = new JsonArrayRequest(url2,jsonArrayListenerWarehouses,errorListener);
+        requestQueue.add(request);
+
+    }
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        switchActivityIntent.putExtra("FILES_TO_SEND", barcodes);
+        switchActivityIntent.putExtra("FILES_TO_SEND2", whouses);
+        startActivity(switchActivityIntent);
+    }
     ActivityResultLauncher<ScanOptions>barlauncher = registerForActivityResult(new ScanContract(),result -> {
        if(result.getContents()!=null){
            AlertDialog.Builder builder = new AlertDialog.Builder(ScannerActivity.this);
            builder.setTitle("result");
+
+           char[] ch = result.getContents().toCharArray();
+           System.out.println(ch.length);
+           String test = "";
+           for (int i = 0; i<ch.length-1;i++){
+               test+=ch[i]+"";
+           }
+           int i = Integer.parseInt(test);
+           barcodes.add(result.getContents());
            res.setText(result.getContents());
            builder.setMessage(result.getContents());
            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
@@ -54,4 +105,27 @@ public class ScannerActivity extends AppCompatActivity {
            }).show();
        }
     });
+    private Response.Listener<JSONArray>jsonArrayListenerWarehouses = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+
+            for(int i = 0; i<response.length(); i++){
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String code = object.getString("warehouseCode");
+                    whouses.add(code);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    };
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error",error.getMessage());
+        }
+    };
 }
